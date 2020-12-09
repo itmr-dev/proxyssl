d9658db615a135fb410551c65dd57b7bec04a7be
craftycram <github@craftycram.net>
2020-12-09 21:38:21 +0100

    removed sudo cd in oneline

--- oneline-install.sh
+++ oneline-install.sh
@@ -3,6 +3,6 @@ echo "downloading files"
 cd /tmp/
 sudo rm -rf /tmp/itmr-proxyssl/
 sudo git clone https://github.com/itmr-dev/itmr-proxyssl.git /tmp/itmr-proxyssl/
-sudo cd /tmp/itmr-proxyssl
+cd /tmp/itmr-proxyssl
 echo "starting install"
 sudo bash ./install.sh
