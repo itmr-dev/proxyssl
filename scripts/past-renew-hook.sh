0ecebe09d02e1796583967d275c9337b20b9e39d
craftycram <github@craftycram.net>
2021-02-05 17:20:00 +0100

    changed past renew script

--- scripts/past-renew-hook.sh
+++ scripts/past-renew-hook.sh
@@ -2,4 +2,4 @@
 pm2 restart all --update-env
 pm2 save
 bash -c "cat /etc/letsencrypt/live/{{mainDomain}}/fullchain.pem /etc/letsencrypt/live/{{mainDomain}}/privkey.pem > /etc/ssl/haproxycert/haproxy.pem"
-service haproxy reload
+service haproxy restart
