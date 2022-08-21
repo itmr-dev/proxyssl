c795cabddf84e65515758887397c64f8c934b41d
Marc Rufeis <github@craftycram.net>
2022-08-21 02:18:14 +0200

    create no clear script

---
+++ oneline-install-noclear.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "getting installer"
+echo "changing to temp folder"
+cd /tmp/
+echo "removing old cached installer versions"
+sudo rm -rf /tmp/proxyssl/
+echo "checking if git is installed"
+if ! command -v git &> /dev/null
+then
+    echo "git not found installing now"
+    sudo apt install git -y
+else
+    echo "git requirement satisfied"
+fi
+echo "cloning repository to tmp"
+sudo git clone https://github.com/itmr-dev/proxyssl.git /tmp/proxyssl/
+echo "changing to installer directory"
+cd /tmp/proxyssl
+echo "starting install"
+sudo bash ./install.sh
