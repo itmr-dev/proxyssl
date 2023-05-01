30ccee5511899f25eb94b15609f24c786038d095
Marc Rufeis <github@craftycram.net>
2023-05-02 00:54:15 +0200

    fix renew for cloudflare

--- scripts/pre-renew-hook.sh
+++ scripts/pre-renew-hook.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Renew the certificate
-certbot renew --force-renewal --tls-sni-01-port=8888
+certbot renew --dns-cloudflare --dns-cloudflare-credentials /opt/.secrets/certbot/cloudflare.ini --dns-cloudflare-propagation-seconds 30
